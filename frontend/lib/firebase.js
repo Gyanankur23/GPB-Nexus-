3f2e22862847124e9bf80fbfcc2926474dbca34e
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-05-27 23:16:23 +0530

    Update firebase.js

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -1,1 +1,21 @@
+// lib/firebase.js
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
