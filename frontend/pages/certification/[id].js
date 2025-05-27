9543e72b5ea02e8909ec6bffea76ab3825a530d5
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-05-27 23:22:26 +0530

    Update [id].js

--- frontend/pages/certification/[id].js
+++ frontend/pages/certification/[id].js
@@ -1,1 +1,23 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { db } from "../../lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import Certificate from "../../components/Certificate";
 
+export default function CertificationPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [roadmap, setRoadmap] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getDoc(doc(db, "roadmaps", id)).then(snap => {
+        if (snap.exists()) setRoadmap(snap.data());
+      });
+    }
+  }, [id]);
+
+  if (!roadmap) return <p>Loading certificate...</p>;
+
+  return <Certificate roadmapTitle={roadmap.title} />;
+}
