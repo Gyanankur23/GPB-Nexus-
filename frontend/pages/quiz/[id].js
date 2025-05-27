bf78ffe63994b67f6d06677d1d04a01a3a3cc9be
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-05-27 23:22:08 +0530

    Update [id].js

--- frontend/pages/quiz/[id].js
+++ frontend/pages/quiz/[id].js
@@ -1,1 +1,9 @@
+import { useRouter } from "next/router";
+import Quiz from "../../components/Quiz";
 
+export default function QuizPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  if (!id) return <p>Loading...</p>;
+  return <Quiz roadmapId={id} />;
+}
