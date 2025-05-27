93538685f4e29ffa5221ff9babae60d831fa8c44
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-05-27 23:20:46 +0530

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,1 +1,13 @@
+import '../styles/globals.css';
+import { ThemeProvider } from '../lib/theme';
+import Layout from '../components/Layout';
 
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
+}
